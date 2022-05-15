62d689fd0dd35b1796ac16c64155f6f74fc4d094
Derek Chou <jo.cordero.it@gmail.com>
2022-05-14 17:51:04 -0700

    init with basic structure

---
+++ src/components/Prescreen/Prescreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Prescreen: React.FC = () =>{
+
+    return (
+        <React.Fragment>
+            <h2>Prescreen</h2>
+        </React.Fragment>
+    )
+}
+
+export default Prescreen;
